1965df40bd6587a5081576421aee8eda180d1316
ywalakamar <barxwells@gmail.com>
2023-06-14 22:09:38 +0300

    chore: application setup

---
+++ src/main/java/com/tuts/security/App.java
@@ -0,0 +1,13 @@
+package com.tuts.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+}
