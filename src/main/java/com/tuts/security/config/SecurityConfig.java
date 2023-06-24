27d9e48486d2da1407fe6884815ad858bd3e335d
ywalakamar <barxwells@gmail.com>
2023-06-24 17:47:03 +0300

    refactor(JwtFilter): endpoint request matcher

--- src/main/java/com/tuts/security/config/SecurityConfig.java
+++ src/main/java/com/tuts/security/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
                 // .authorizeHttpRequests(
                 // auth -> auth.anyRequest().permitAll())
                 .authorizeHttpRequests(
-                        authorize -> authorize.requestMatchers(HttpMethod.GET, "/api/v1/users").permitAll()
+                        authorize -> authorize.requestMatchers(HttpMethod.GET, "/api/v1/**").permitAll()
                                 .requestMatchers("/api/v1/auth/**").permitAll()
                                 .anyRequest().authenticated()
 
