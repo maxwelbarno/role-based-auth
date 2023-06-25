55269f1a07ecfac468f589102b9d74cc9e5a4a3c
ywalakamar <barxwells@gmail.com>
2023-06-25 22:46:52 +0300

    fixes: clear input form log errors

--- client/src/features/login/Login.js
+++ client/src/features/login/Login.js
@@ -3,27 +3,27 @@ import "./Login.css";
 const Login = () => {
   return (
     <div className="container">
-      <header class="header">
+      <header className="header">
         <h1>Sign In</h1>
       </header>
-      <form action="" class="form">
-        <div class="form-row">
+      <form action="" className="form">
+        <div className="form-row">
           <input
             type="email"
-            name="full-name"
-            id="full-name"
+            name="email"
+            id="email"
             placeholder="Enter Email Address"
           />
         </div>
-        <div class="form-row">
+        <div className="form-row">
           <input
             type="password"
-            name="full-name"
-            id="full-name"
+            name="password"
+            id="password"
             placeholder="Enter Password"
           />
         </div>
-        <div class="form-row">
+        <div className="form-row">
           <input type="button" name="button" value="Submit" id="submit" />
         </div>
       </form>
